74c01ed85a632d83f0357d1bb1177a8bb9679460
Tarun-108 <tarunshrivastava0108@gmail.com>
2021-02-23 00:20:18 +0530

    xml done for login and register activity

---
+++ app/src/main/java/com/example/o_x/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.o_x;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
