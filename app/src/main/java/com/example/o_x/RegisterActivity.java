9ab4c997a2b5e1b40f3c1ae7d3e7d991c83a72aa
Tarun-108 <tarunshrivastava0108@gmail.com>
2021-02-26 17:18:59 +0530

    hideSoftKeyboard done also project added to firebase.

--- app/src/main/java/com/example/o_x/RegisterActivity.java
+++ app/src/main/java/com/example/o_x/RegisterActivity.java
@@ -2,9 +2,11 @@ package com.example.o_x;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -17,4 +19,9 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
     }
 
+    public void hidesoftkeyboard(View view) {
+
+        InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        im.hideSoftInputFromWindow(view.getWindowToken(), 0 );
+    }
 }
